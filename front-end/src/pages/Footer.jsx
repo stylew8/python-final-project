6805af1a377292e3d7eba91cf105a0a177372071
Nojus <78691849+NoahIT@users.noreply.github.com>
2024-11-26 19:31:09 +0000

    landing page

--- front-end/src/pages/Footer.jsx
+++ front-end/src/pages/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../styles/footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div class="def-footer">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center border-top">
+        <p class=" nav-link">© 2024 TAMO KLONAS, Inc</p>
+
+        <ul class="nav justify-content-end">
+          <li class="nav-item"><a href="#" class="nav-link px-2 ">Tvarkaraštis</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 ">DUK</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 ">Privatumo politika</a></li>
+        </ul>
+      </footer>
+    </div>
   )
 }
 
--- front-end/src/pages/Main.jsx
+++ front-end/src/pages/Main.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import '../styles/index.css'
 
+import studentImg from '../images/student.png';
+import teacherImg from '../images/teacher.png';
+import adminImg from '../images/whiteboard.png';
 
 export default function Main() {
-
     return (
-        <div>
-            
-        </div>
+        <main className="landing-page">
+            <h1>Prašome prisijungti prie paskyros</h1>
+            <section>
+                <a className="student-log" href="#">
+                    <img src={studentImg} alt="Logo" />
+                    <h2>Studentas</h2>
+                </a>
+                <a className="teacher-log"  href="#">
+                    <img src={teacherImg} alt="Logo" />
+                    <h2>Mokytojas</h2>
+                </a>
+                <a className="admin-log"  href="#">
+                    <img src={adminImg} alt="Logo" />
+                    <h2>Administratorius</h2>
+                </a>
+            </section>
+        </main>
     );
 }
